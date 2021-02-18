d97d5129f907f1f8fa5317880d7738d4f00d9217
Gerard Pitarque <gpitarque@iesebre.com>
2021-02-18 16:25:34 +0100

    first commit

---
+++ src/codifont/Practica2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codifont;
+import static metodes.Cadena.juntaSaltejats;
+
+/**
+ *
+ * @author alumne
+ */
+public class Practica2 {
+    
+    public static void main(String[] args) {
+        //Fem les proves per veure si funciona el mètode correctament
+        System.out.println(juntaSaltejats(-25));
+        System.out.println(juntaSaltejats(3));
+        System.out.println(juntaSaltejats(0, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(1, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(2, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(3, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(4, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(5, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(6, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(7, "La","teva","resposta","no","és","la","correcta."));
+        System.out.println(juntaSaltejats(8, "La","teva","resposta","no","és","la","correcta."));
+    }
+}
